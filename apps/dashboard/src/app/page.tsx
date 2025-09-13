b9981413dcf6ab48ac19a625e071f1b37fb4c1e2
lanstheprodigy <lanstheprodigy@icloud.com>
2025-09-13 14:37:30 +0700

    deploy (ci-cd): create a cicd pipeline

--- apps/dashboard/src/app/page.tsx
+++ apps/dashboard/src/app/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <div>Hello World CI/CD Works!, Newest Change</div>
       <Link href="/client-force">Client Force</Link>
       <Link href="/client-not-force">Client Not Force</Link>
       <Link href="/server-force">Server Force</Link>

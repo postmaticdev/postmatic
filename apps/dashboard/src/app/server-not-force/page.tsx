d4f43d6cb4755ce2dcfbbdbeed805df4dccba2dd
lanstheprodigy <lanstheprodigy@icloud.com>
2025-09-13 16:00:08 +0700

    deploy (ci-cd): create a cicd pipeline

--- apps/dashboard/src/app/server-not-force/page.tsx
+++ apps/dashboard/src/app/server-not-force/page.tsx
@@ -1,13 +1,15 @@
 export default function Home() {
+  const NEXT_PUBLIC_API_ORIGIN = process.env.NEXT_PUBLIC_API_ORIGIN || "env not detected / set";
+  const API_ORIGIN = process.env.API_ORIGIN || "env not detected / set";
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       server not force
       <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
         NEXT_PUBLIC_API_ORIGIN:{" "}
-        {process.env.NEXT_PUBLIC_API_ORIGIN || "env not detected / set"}
+        {NEXT_PUBLIC_API_ORIGIN}
       </code>
       <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-        API_ORIGIN: {process.env.API_ORIGIN || "env not detected / set"}
+        API_ORIGIN: {API_ORIGIN}
       </code>
     </div>
   );

6bd9e224dbc13f871e25438a196161792134d2fa
lanstheprodigy <lanstheprodigy@icloud.com>
2025-09-18 20:15:56 +0700

    bugfix (purchase): remove use auth on webhook midtrans

--- apps/backend-service/src/routes/purchase/index.ts
+++ apps/backend-service/src/routes/purchase/index.ts
@@ -4,12 +4,23 @@ import businessPurchaseRoute from "./business";
 import checkoutPurchaseRoute from "./checkout";
 import webhookRoute from "./webhook";
 import { useAuth } from "../../middleware/use-auth";
+import { useRateLimiter } from "../../middleware/use-rate-limiter";
 
 const purchaseRoute = express.Router();
 
-purchaseRoute.use("/user", useAuth, userPurchaseRoute);
-purchaseRoute.use("/business", useAuth, businessPurchaseRoute);
-purchaseRoute.use("/checkout", useAuth, checkoutPurchaseRoute);
+purchaseRoute.use("/user", useAuth, useRateLimiter.auth, userPurchaseRoute);
+purchaseRoute.use(
+  "/business",
+  useAuth,
+  useRateLimiter.auth,
+  businessPurchaseRoute
+);
+purchaseRoute.use(
+  "/checkout",
+  useAuth,
+  useRateLimiter.auth,
+  checkoutPurchaseRoute
+);
 purchaseRoute.use("/webhook", webhookRoute);
 
 export default purchaseRoute;
